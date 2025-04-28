eac9b92ba6ce40a2994da53707a04f9eb45496be
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-28 10:13:22 +0700

    Update task_handler.cpp

--- PROJECT01/Src/task_handler.cpp
+++ PROJECT01/Src/task_handler.cpp
@@ -257,6 +257,7 @@ void Task_Setup_Communication_Handler(void *pvParameters)
     while (1)
     {
         xTaskNotifyWait(0, 0, NULL, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(10));
         //sprintf((char*)DebugData, "Task Setup Communication: %d, %d\n", SetCom_NotifyNum, selfInf.slot);
         //HAL_UART_Transmit(&huart1, DebugData, strlen((char*)DebugData), 100);
         switch (SetCom_NotifyNum)
